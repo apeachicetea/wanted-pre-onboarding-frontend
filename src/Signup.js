180c61a3fc332227a1105f2d5f0b6aa2e7db663e
apeachicetea <eodbszla@gmail.com>
2023-08-04 18:06:15 +0900

    Add basic signup page

--- src/Signup.js
+++ src/Signup.js
@@ -1,7 +1,111 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useState, useEffect } from "react";
+import styled from "styled-components";
+import axios from "axios";
+
+const Container = styled.div`
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Wrapper = styled.div`
+  width: 200px;
+  height: 200px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+const EmailInput = styled.input`
+  width: 100%;
+  height: 20px;
+  margin-bottom: 5px;
+`;
+
+const PasswordInput = styled(EmailInput)``;
+
+const SignupBtn = styled.button`
+  width: 80px;
+  height: 25px;
+  cursor: pointer;
+`;
 
 function Signup() {
-  return <React.Fragment>Signup Page</React.Fragment>;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [isBtnDisabled, setIsBtnDisabled] = useState(true);
+  const baseURL = "https://www.pre-onboarding-selection-task.shop";
+
+  const handleSubmit = async () => {
+    try {
+      const response = await axios.post(
+        `${baseURL}/auth/signup`,
+        {
+          email,
+          password,
+        },
+        {
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+      console.log(response);
+    } catch (error) {
+      console.error("Error sending POST request:", error);
+    }
+  };
+
+  useEffect(() => {
+    if (email.includes("@") && password.length >= 8) {
+      setIsBtnDisabled(false);
+    } else {
+      setIsBtnDisabled(true);
+    }
+  }, [email, password]);
+
+  const onChangeEmail = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const onChangePassword = (e) => {
+    setPassword(e.target.value);
+  };
+
+  const onClickBtn = () => {
+    handleSubmit();
+  };
+
+  return (
+    <React.Fragment>
+      <Container>
+        <Wrapper>
+          <EmailInput
+            type="email"
+            data-testid="email-input"
+            placeholder="write your email here"
+            onChange={onChangeEmail}
+          />
+          <PasswordInput
+            type="password"
+            data-testid="password-input"
+            placeholder="write your password here"
+            onChange={onChangePassword}
+          />
+          <SignupBtn
+            disabled={isBtnDisabled}
+            data-testid="signup-button"
+            onClick={onClickBtn}
+          >
+            Signin
+          </SignupBtn>
+        </Wrapper>
+      </Container>
+    </React.Fragment>
+  );
 }
 
 export default Signup;
