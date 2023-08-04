d5ffa224b8342964fbfa338944fab1ba5f295919
apeachicetea <eodbszla@gmail.com>
2023-08-04 19:01:22 +0900

    Add Router page

--- src/App.js
+++ src/App.js
@@ -1,19 +1,10 @@
 import React, { Fragment } from "react";
-import { Routes, Route } from "react-router-dom";
-import Home from "./Home";
-import Signup from "./Signup";
-import Signin from "./Signin";
-import Todo from "./Todo";
+import Router from "./Router";
 
 function App() {
   return (
     <React.Fragment>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/signin" element={<Signin />} />
-        <Route path="/todo" element={<Todo />} />
-      </Routes>
+      <Router />
     </React.Fragment>
   );
 }
---
+++ src/Router.js
@@ -0,0 +1,28 @@
+import React, { Fragment, useEffect } from "react";
+import { Routes, Route, useNavigate } from "react-router-dom";
+import Home from "./Home";
+import Signup from "./Signup";
+import Signin from "./Signin";
+import Todo from "./Todo";
+
+function Router() {
+  const navigate = useNavigate();
+  const isAuthorized = localStorage.getItem("accessToken");
+
+  useEffect(() => {
+    isAuthorized ? navigate("/") : navigate("/signin");
+  }, [isAuthorized, navigate]);
+
+  return (
+    <React.Fragment>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/signin" element={<Signin />} />
+        <Route path="/todo" element={<Todo />} />
+      </Routes>
+    </React.Fragment>
+  );
+}
+
+export default Router;
