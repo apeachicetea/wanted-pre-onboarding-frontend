60ab2f62f929e009f1c6d2f7cd334d82f36b3b65
apeachicetea <eodbszla@gmail.com>
2023-08-05 20:47:55 +0900

    Add todo link button

--- src/Home.js
+++ src/Home.js
@@ -1,15 +1,31 @@
 import React, { Fragment, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
+import styled from "styled-components";
+
+const Btn = styled.button`
+  cursor: pointer;
+`;
+
+const Text = styled.h1``;
 
 function Home() {
   const navigate = useNavigate();
+
+  const handleClick = () => {
+    navigate("/todo");
+  };
 
   useEffect(() => {
     const isAuthorized = localStorage.getItem("accessToken");
     if (!isAuthorized) navigate("/signin");
   }, [navigate]);
 
-  return <Fragment>Home Page</Fragment>;
+  return (
+    <Fragment>
+      <Btn onClick={handleClick}>TodoList</Btn>
+      <Text>Home Page</Text>
+    </Fragment>
+  );
 }
 
 export default Home;
